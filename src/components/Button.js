440c1372c25dcce4b2ecc27a8fdffbb6460532fc
avfrontend <avraam.kastrappis@gmail.com>
2021-08-15 23:04:20 +0300

    create react static website

--- src/components/Button.js
+++ src/components/Button.js
@@ -13,11 +13,9 @@ export const Button = ({
     buttonStyle,
     buttonSize
 }) => {
-    const checkButtonStyle = STYLES.includes(buttonStyle)
-    ? buttonStyle
-    : STYLES[0];
-    
-    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
+
+    const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
+    const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
     return (
         <Link to ="/sign-up" className="btn-mobile">
